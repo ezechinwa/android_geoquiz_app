f64a2df297d1bdb9be637674cf2c31f774031556
Williams Chinwa <chinwawilliams@gmail.com>
2023-04-30 16:58:04 +0100

    added a scoring mechanism to the app

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -4,6 +4,8 @@ package com.example.geoquiz
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.annotation.StringRes
 import com.example.geoquiz.databinding.ActivityMainBinding
@@ -11,23 +13,26 @@ import com.example.geoquiz.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
 
-    private val questionBank: List<Question>  = listOf(
-        Question(R.string.question_cariberra, true),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_ocean, true),
+    private var questionBank: List<Question>  = listOf(
+        Question(R.string.question_cariberra, true, false, 10),
+        Question(R.string.question_africa, false, false, 10),
+        Question(R.string.question_ocean, true, false, 5),
     )
     private var currentIndex: Int = 0
-    private val TAG:String = "MainActivity"
+    private var currentScore: Int = 0
+    private val TAG:String = "MainActivityLogcat"
     private lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG,"onCreate(savedInstanceState: Bundle?) called")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         @StringRes val questionTextResId: Int = questionBank[currentIndex].textResId
         binding.questionText.setText(questionTextResId)
+        binding.scoreText.setText(currentIndex.toString())
 
         binding.questionText.setOnClickListener{
             updateCounter()
@@ -36,6 +41,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.trueButton.setOnClickListener {
             // Do something when the trueButton is clicked
+
             checkAnswer(true)
             updateCounter()
             updateQuestion()
@@ -51,9 +57,9 @@ class MainActivity : AppCompatActivity() {
             updateCounter()
             updateQuestion()
         }
-
-
     }
+
+
 
     private fun toastMessage(context: Context, @StringRes resourceId: Int, duration: Int ):Unit {
         Toast.makeText(context, resourceId, duration).show()
@@ -63,15 +69,52 @@ class MainActivity : AppCompatActivity() {
         currentIndex = (currentIndex + 1) % questionBank.size
     }
     private fun updateQuestion(): Unit{
+        var questionAnsered = questionBank[currentIndex].answered
+        if (questionAnsered) {
+            binding.trueButton.visibility = View.GONE
+            binding.falseButton.visibility = View.GONE
+        }
+
         @StringRes val questionTextResId: Int = questionBank[currentIndex].textResId
         binding.questionText.setText(questionTextResId)
+
+    }
+
+    private fun updateAnsweredStatus(): Unit {
+        Log.i(TAG, "updateAnsweredStatus called")
+        var questionAnsered = questionBank[currentIndex].answered
+        Log.i(TAG, "updateAnsweredStatus before Update "+ questionAnsered.toString())
+
+        questionBank[currentIndex].answered = true
+         questionAnsered = questionBank[currentIndex].answered
+
+        Log.i(TAG, "updateAnsweredStatus after Update "+ questionAnsered.toString())
     }
 
     private fun checkAnswer(userAnswer: Boolean): Unit{
        if (userAnswer == questionBank[currentIndex].answer) {
            toastMessage(this,R.string.correct_toast_text,Toast.LENGTH_SHORT)
+           currentScore = currentScore + questionBank[currentIndex].point
        }else{
            toastMessage(this,R.string.incorrect_toast_text, Toast.LENGTH_LONG)
        }
+
+       if(currentIndex == ( questionBank.size - 1)){
+           binding.scoreLabelText.setText("Total Score: ")
+           var total: Int = 0 ;
+           for(question in questionBank){
+               total = total + question.point
+           }
+           val percentageScore:Double = ( (currentScore.toDouble() /total.toDouble())) * 100
+           val totalScoreMessage: String = "You scored : "+ percentageScore + "%"
+
+           binding.scoreText.setText(totalScoreMessage)
+           binding.scoreLabelText.setText("")
+           updateAnsweredStatus()
+       }else{
+           binding.scoreText.setText(currentScore.toString())
+           updateAnsweredStatus()
+       }
+
     }
 }
